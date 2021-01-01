5a5023ca0b9f4a51e6f22c33f8ca2192dfaf9aed
miracle <378479645@qq.com>
2021-01-01 16:43:29 +0800

    revert code to nothing header

--- include/uapi/asm-generic/Kbuild.asm
+++ include/uapi/asm-generic/Kbuild.asm
@@ -1,9 +1,4 @@
 #
 # Headers that are mandatory in usr/include/asm/
 #
-mandatory-y += bitsperlong.h
-mandatory-y += errno.h
-mandatory-y += posix_types.h
-mandatory-y += swab.h
-mandatory-y += types.h
-mandatory-y += unistd.h
+mandatory-y += test.h
--- init/main.c
+++ init/main.c
@@ -1,7 +1,4 @@
 #include <linux/kconfig.h>
-#include <linux/cache.h>
-
-unsigned long __cacheline_aligned boot_args[4];
 
 int start_kernel(void)
 {
