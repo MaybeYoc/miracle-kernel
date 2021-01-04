b2f9a8e76a746a38e995df4d5679a92e8ae3c46e
miracle <yi.zhang@bst.ai>
2021-01-04 16:37:48 +0800

    nine commit head.S to start_kernel

--- init/main.c
+++ init/main.c
@@ -1,8 +1,16 @@
+
+#include <linux/kernel.h>
 #include <linux/kconfig.h>
 #include <linux/types.h>
 #include <linux/compiler.h>
 
 int start_kernel(void)
 {
-	return 0;
+	u64 aa, bb;
+
+	aa = 2;
+
+	bb = 3;
+
+	return aa + bb;
 }
