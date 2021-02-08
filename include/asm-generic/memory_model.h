0b18594fc47812e89433ad994773448c5fe21bc8
miracle <yi.zhang@bst.ai>
2021-02-08 18:31:19 +0800

    Commit pfn ops

--- include/asm-generic/memory_model.h
+++ include/asm-generic/memory_model.h
@@ -1,15 +1,19 @@
 #ifndef __ASM_MEMORY_MODEL_H
 #define __ASM_MEMORY_MODEL_H
 
-#ifndef __ASSEMBLY__
-/* TODO */
-/*
- * Note: section's mem_map is encoded to reflect its start_pfn.
- * section[i].section_mem_map == mem_map's address - start_pfn;
- */
-#define __page_to_pfn(pg)
+#include <linux/pfn.h>
 
-#define __pfn_to_page(pfn)
+#ifndef __ASSEMBLY__
+
+/* memmap is virtually contiguous.  */
+#define __pfn_to_page(pfn)	(vmemmap + (pfn))
+#define __page_to_pfn(page)	(unsigned long)((page) - vmemmap)
+
+/*
+ * Convert a physical address to a Page Frame Number and back
+ */
+#define	__phys_to_pfn(paddr)	PHYS_PFN(paddr)
+#define	__pfn_to_phys(pfn)	PFN_PHYS(pfn)
 
 #define page_to_pfn __page_to_pfn
 #define pfn_to_page __pfn_to_page
