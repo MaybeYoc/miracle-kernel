5ba67e13228e54aebe101f42f3477e68af79b3d1
miracle <yi.zhang@bst.ai>
2021-02-20 13:48:23 +0800

    Update FORCE_MAX_ZONEORDER

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -4,7 +4,13 @@
 
 #ifndef __ASSEMBLY__
 
+/* Free memory management - zoned buddy allocator.  */
+#ifndef CONFIG_FORCE_MAX_ZONEORDER
 #define MAX_ORDER 11
+#else
+#define MAX_ORDER CONFIG_FORCE_MAX_ZONEORDER
+#endif
+#define MAX_ORDER_NR_PAGES (1 << (MAX_ORDER - 1))
 
 #define pageblock_order		(MAX_ORDER-1)
 
