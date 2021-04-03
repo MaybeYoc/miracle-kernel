64d279e6aaeba8abfc36a4c6178319e6433e70d2
miracle <378479645@qq.com>
2021-04-03 20:16:52 +0800

    revert gicv2 qemu test

---
+++ tools/qemu/gicv2_start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+qemu-system-aarch64 	\
+	-cpu cortex-a57 -machine type=virt,gic-version=2 -smp 8 -m 1024M 	\
+	-append "rdinit=/linuxrc console=ttyAMA0 earlycon=115200"	\
+	-kernel ../../arch/arm64/boot/Image 	\
+	-device virtio-scsi-device 	\
+	-nographic
