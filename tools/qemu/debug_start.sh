fd2b8baac5cfe1ae5a3c1eaa84375bc2aee66b7f
miracle <378479645@qq.com>
2021-04-03 22:10:56 +0800

    fix gicv2

--- tools/qemu/debug_start.sh
+++ tools/qemu/debug_start.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-qemu-system-aarch64 -cpu cortex-a57 -machine type=virt,gic-version=3 	\
+qemu-system-aarch64 -cpu cortex-a57 -machine type=virt,gic-version=2 	\
 		-append "rdinit=/linuxrc console=ttyAMA0 earlycon=115200"	\
 		-kernel ../../arch/arm64/boot/Image	\
 		-device virtio-scsi-device	\
-		-smp cores=4,threads=5	\
-		-m 2G	\
-        -object memory-backend-ram,id=mem0,size=512M \
-        -object memory-backend-ram,id=mem1,size=128M \
+		-smp cores=4,threads=2	\
+		-m 4G	\
+        -object memory-backend-ram,id=mem0,size=1G \
+        -object memory-backend-ram,id=mem1,size=512M \
         -object memory-backend-ram,id=mem2,size=256M \
-        -object memory-backend-ram,id=mem3,size=1G \
-		-object memory-backend-ram,id=mem4,size=128M \
-		-numa node,memdev=mem0,cpus=8-11,nodeid=0 \
-        -numa node,memdev=mem1,cpus=4-7,cpus=12,nodeid=1 \
-		-numa node,memdev=mem2,cpus=13-15,nodeid=2 \
-        -numa node,memdev=mem3,cpus=16-19,nodeid=3 \
-		-numa node,memdev=mem4,cpus=0-3,nodeid=4 \
+        -object memory-backend-ram,id=mem3,size=2G \
+		-object memory-backend-ram,id=mem4,size=256M \
+		-numa node,memdev=mem0,cpus=5-6,nodeid=0 \
+        -numa node,memdev=mem1,cpus=3,cpus=7,nodeid=1 \
+		-numa node,memdev=mem2,cpus=4,nodeid=2 \
+        -numa node,memdev=mem3,cpus=0-1,nodeid=3 \
+		-numa node,memdev=mem4,cpus=2,nodeid=4 \
 		-numa dist,src=0,dst=0,val=10	\
 		-numa dist,src=0,dst=1,val=15	\
 		-numa dist,src=0,dst=2,val=20	\
